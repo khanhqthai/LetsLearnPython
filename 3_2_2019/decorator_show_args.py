e05604dab91a091fc20fe8d0baba0e17e620eae7
khanh q thai <khanhqthai@gmail.com>
2019-03-02 20:58:42 -0800

    learn python classes with world of warcraft

--- 3_2_2019/decorator_show_args.py
+++ 3_2_2019/decorator_show_args.py
@@ -13,5 +13,17 @@ do_nothing(1, 2, 3,a="hi",b="bye")
 from functools import wraps
 
 
-def show_args():
-    pass
+def show_args(fn):
+    #wraps keep meta data
+    @wraps(fn)
+    def wrapper(*args,**kwargs):
+        '''Wrapper function for show_args'''
+        return fn(*args,**kwargs)
+
+    return wrapper
+
+@show_args
+def do_nothing(*args,**kwargs):
+    print((*args))
+    print(dict(**kwargs))
+do_nothing(1,2,3, a="hi",b="bye")
