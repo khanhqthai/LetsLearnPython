49709b3f55c0167f9fb001bb5fd31fa97b513839
khanh q thai <khanhqthai@gmail.com>
2019-03-05 23:36:32 -0800

    playing with class methods

--- WoW/azeroth.py
+++ WoW/azeroth.py
@@ -8,7 +8,7 @@ class AzerothCitizen:
     RACE = ("Orc", "Human", "Troll", "Undead", "Gnome", "Tauren", "Night Elf", "Other")
     SEX = ("Male", "Female")
 
-    def __init__(self, name: str, race: RACE, sex: SEX, job: JOBS, allegiance: ALLEGIANCE):
+    def __init__(self, name: str, race: RACE, sex: SEX, job: JOBS):
         self._name = name
         # race value must be in RACE
         if race not in self.RACE:
@@ -18,10 +18,9 @@ class AzerothCitizen:
 
         self.job = job
         self.sex = sex
-        self.allegiance = allegiance
-
+    #__repr__ should be unambigous, this will help developer/us to debug code
     def __repr__(self):
-        return f"Azeroth citizen object -  {self._name}, {self._race}, {self.job}, {self.sex}, {self.allegiance}"
+        return f"{self.__class__.__name__}({self._name}, {self._race}, {self.job}, {self.sex})"
 
     def battle_cry(self):
         return f"{self._name} yells: Charge!!"
@@ -36,21 +35,36 @@ class Orc(AzerothCitizen):
     Creates Azeroth Citizen Orc
     """
     def __init__(self, name: str, sex: str, job: str):
-        super().__init__(name, "Orc", sex, job, "The Horde")
+
+        # _denotes class private variable - Python does not enforce, but rather it is convention
+        self._allegiance = "The Horde"
+        super().__init__(name, "Orc", sex, job)
+    def __repr__(self):
+        return f"{self.__class__.__name__} class instance - ({self._name}, {self._race}, {self.job}, {self._allegiance})"
 
     def battle_cry(self):
         return f"{self.get_name()} yells: FOR THE HORDE!!"
 
+    @classmethod
+    def thrall(cls):
+        return cls("Thrall", "Male", "Shaman")
 
 class Human(AzerothCitizen):
     """
     Creates Azeroth Citizen Human
     """
     def __init__(self, name: str, sex: str, job: str):
-        super().__init__(name, "Human", sex, job, "The Alliance")
+        self._allegiance = "The Alliance"
+        super().__init__(name, "Human", sex, job)
+    def __repr__(self):
+        return f"{self.__class__.__name__} class instance - ({self._name}, {self._race}, {self.job}, {self._allegiance})"
 
     def battle_cry(self):
         return f"{self.get_name()} yells: FOR THE ALLIANCE!!"
+
+    @classmethod
+    def jaina(cls):
+        return cls("Jaina", "Female", "Mage")
 
 class Leader(AzerothCitizen):
     """
@@ -60,20 +74,22 @@ class Leader(AzerothCitizen):
         self.tribe=tribe
         super().__init__(name, race, sex, job, allegiance)
 
-    @classmethod
-    def thrall(cls):
-        return Orc("Thrall", "Male", "Warrior")
+    def __repr__(self):
+        return f"{self.__class__.__name__} class instance ({self._name}, {self._race}, {self.allegiance}, {self.tribe})"
 
-    @classmethod
-    def jaina(cls):
-        return Human("Jaina", "Female", "Mage")
+
+
+
 
 
 if __name__ == "__main__":
 
-    thrall = Leader.thrall()
+    thrall = Orc.thrall()
+    print(thrall)
     print(thrall.battle_cry())
 
-    jaina = Leader.jaina()
+    jaina = Human.jaina()
+    print(jaina)
     print(jaina.battle_cry())
+
 
